21e776a3bc0f28686bb88b3d1dbb4d5efc31b8d7
Burt Beckwith <beckwithb@vmware.com>
2013-01-14 01:35:04 -0500

    added version to json2.js filename

--- grails-app/conf/BackboneResources.groovy
+++ grails-app/conf/BackboneResources.groovy
@@ -1,12 +1,11 @@
 modules = {
 
     json2 {
-        resource url: '/js/json2.js'
+        resource url: '/js/json2-2012-10-08.js'
     }
 
     backbone {
         dependsOn 'underscore', 'json2'
         resource url: '/js/backbone.js'
     }
-
 }
